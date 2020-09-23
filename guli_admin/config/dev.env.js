b191189b737e50d0ee0a135287637439f1ffef20
吴涛 <13155171521@163.com>
2020-09-23 21:44:20 +0800

    科目上传

--- guli_admin/config/dev.env.js
+++ guli_admin/config/dev.env.js
@@ -5,4 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   BASE_API: '"http://127.0.0.1:8110"',
+  OSS_PATH: '"https://wutao-file.oss-cn-shenzhen.aliyuncs.com"'
 })
